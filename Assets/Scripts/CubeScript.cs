e64b19766040d0f5b0c90363b12b8fe445b25ec1
rexark <nazeel.mail2me@gmail.com>
2018-02-06 16:00:28 +0530

    v0.12 - Animated UI

--- Assets/Scripts/CubeScript.cs
+++ Assets/Scripts/CubeScript.cs
@@ -5,9 +5,11 @@ using UnityEngine.UI;
 public class CubeScript : MonoBehaviour {
 
     public Text CubeText;
+    public string objType;
+    public Animator anim;
 	// Use this for initialization
 	void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
@@ -23,5 +25,18 @@ public class CubeScript : MonoBehaviour {
     public void displayGUI(string message)
     {
         CubeText.text = message;
+        anim.Play("fade in");
+        objType = message;
     }
+
+    public void displayInfo()
+    {
+        switch (objType)
+        {
+            case "Cube": //display info about cube
+                break;
+
+        }
+    }
+
 }
