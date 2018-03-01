2515234742cc4df8beff1da8f92e3619ae07cb92
rexark <nazeel.mail2me@gmail.com>
2018-03-01 15:48:27 +0530

    v0.19 - Walking Avatar

---
+++ Assets/Scripts/HeadScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadScript : MonoBehaviour {
+
+    private GameObject cam;
+    private int val;
+
+	// Use this for initialization
+	void Start ()
+    {
+        cam = GameObject.FindGameObjectWithTag("MainCamera");
+        if (gameObject.name.Equals("Helmet"))
+            val = 1;
+        else if (gameObject.name.Equals("Body"))
+            val = 0;
+
+    }
+	
+	// Update is called once per frame
+	void LateUpdate ()
+    {
+        
+            transform.rotation = new Quaternion(cam.transform.rotation.x*val, cam.transform.rotation.y, cam.transform.rotation.z*val, 1);
+    }
+}
