77e5c3d1b6f3843e53d41e499564b67185faa1f6
rexark <nazeel.mail2me@gmail.com>
2018-03-15 16:32:11 +0530

    v0.21 - Location Pins

---
+++ Assets/Scripts/LookatScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookatScript : MonoBehaviour {
+
+    private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        transform.LookAt(player.transform);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
